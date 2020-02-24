2d5f3a4dcd1f637cc1c2659ced68fdf283fc9689
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2020-02-24 23:53:54 +0900

    indent

--- app/Console/Commands/StoreCommentsCommand.php
+++ app/Console/Commands/StoreCommentsCommand.php
@@ -6,37 +6,37 @@ use Illuminate\Console\Command;
 
 class StoreCommentsCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'command:name';
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'command:name';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Command description';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+  /**
+   * Create a new command instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    parent::__construct();
+  }
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-        //
-    }
+  /**
+   * Execute the console command.
+   *
+   * @return mixed
+   */
+  public function handle()
+  {
+    //
+  }
 }
